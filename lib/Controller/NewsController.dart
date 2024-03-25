b7383098a50a2fa6c2d07981e211f514fa2bdce2
Gourav Dhiman <techstudy.gourav@gmail.com>
2024-03-25 14:03:09 +0530

    audio issue fixed...& new API integration done...

--- lib/Controller/NewsController.dart
+++ lib/Controller/NewsController.dart
@@ -106,3 +106,4 @@ class NewsController extends GetxController {
     isSpeaking.value = false;
   }
 }
+
--- lib/Pages/ProfilePage.dart
+++ lib/Pages/ProfilePage.dart
@@ -7,10 +7,29 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Profile Page")),
-      body: Column(
-        children: [
-          Container(),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          children: [
+            Container(
+              child: Text(
+                "Subscribe to Our Newsletter!",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            new Image.asset("assets/images/newsletter.jpg"),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                border: Border
+                    .all(), // Optionally, you can specify border properties
+              ),
+              child: TextField(
+                  // TextField properties...
+                  ),
+            )
+          ],
+        ),
       ),
     );
   }
