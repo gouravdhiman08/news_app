e474027db6c5fd528c13e4dab44543c5e863cc8a
Gourav Dhiman <techstudy.gourav@gmail.com>
2024-03-26 11:37:59 +0530

    minor issue fixed....

--- lib/Pages/NewsDetail.dart
+++ lib/Pages/NewsDetail.dart
@@ -7,156 +7,158 @@ import 'package:news_app/Model/NewsModel.dart';
 class NewsDetailPage extends StatelessWidget {
   final NewsModel newsD;
   const NewsDetailPage({
-    super.key,
+    Key? key,
     required this.newsD,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     NewsController newsController = Get.put(NewsController());
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        Get.find<NewsController>().stop();
-                        Get.back();
-                      },
-                      child: Container(
-                        child: const Row(
-                          children: [
-                            Icon(Icons.arrow_back_ios_new),
-                            Text("Back"),
-                          ],
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Container(
-                  decoration: BoxDecoration(
-                    // color: Colors.red,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Row(
+    return WillPopScope(
+      onWillPop: () async {
+        // Stop TTS when using the phone's back button
+        Get.find<NewsController>().stop();
+        return true;
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Row(
                     children: [
-                      Expanded(
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Image.network(
-                            newsD.urlToImage??
-                            "https://duet-cdn.vox-cdn.com/thumbor/0x0:2040x1360/750x500/filters:focal(1020x680:1021x681):format(webp)/cdn.vox-cdn.com/uploads/chorus_asset/file/23262657/VRG_Illo_STK001_B_Sala_Hacker.jpg",
-                            fit: BoxFit.cover,
+                      InkWell(
+                        onTap: () {
+                          // Stop TTS and navigate back
+                          Get.find<NewsController>().stop();
+                          Get.back();
+                        },
+                        child: Container(
+                          child: const Row(
+                            children: [
+                              Icon(Icons.arrow_back_ios_new),
+                              Text("Back"),
+                            ],
                           ),
                         ),
                       )
                     ],
                   ),
-                ),
-                SizedBox(height: 20),
-                Text(
-                  newsD.title!,
-                  // "Midjourney bans all Stability AI employees over alleged data scraping",
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 12),
-                Row(
-                  children: [
-                    Text(
-                      newsD.publishedAt!,
-                      // "2-March-2024",
-                      style: Theme.of(context).textTheme.labelSmall,
-                    )
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 15,
-                      backgroundImage: AssetImage('assets/images/image.png'),
-
-                      // backgroundColor: Colors.purple,
-                      child: Text(
-                        "",
-                        style: const TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
+                  const SizedBox(height: 20),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
                     ),
-                    const SizedBox(width: 10),
-                    Text(
-                      newsD.author ?? "Gourav Dhiman",
-                      style: TextStyle(fontSize: 18, color: Colors.black),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.network(
+                              newsD.urlToImage ??
+                                  "https://duet-cdn.vox-cdn.com/thumbor/0x0:2040x1360/750x500/filters:focal(1020x680:1021x681):format(webp)/cdn.vox-cdn.com/uploads/chorus_asset/file/23262657/VRG_Illo_STK001_B_Sala_Hacker.jpg",
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        )
+                      ],
                     ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.primaryContainer,
-                    borderRadius: BorderRadius.circular(10),
                   ),
-                  // height: 30,
-                  child: Row(
+                  SizedBox(height: 20),
+                  Text(
+                    newsD.title!,
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Row(
                     children: [
-                      Obx(
-                        () => newsController.isSpeaking.value
-                            ? IconButton(
-                                onPressed: () {
-                                  newsController.stop();
-                                },
-                                icon: Icon(
-                                  Icons.stop,
-                                  size: 50,
-                                ),
-                              )
-                            : IconButton(
-                                onPressed: () {
-                                  newsController.speak(
-                                      newsD.description ?? "No Description");
-                                },
-                                icon: Icon(
-                                  Icons.play_arrow_rounded,
-                                  size: 50,
-                                ),
-                              ),
-                      ),
-                      Expanded(
-                          child: Obx(
-                        () => Lottie.asset(
-                          'assets/animation/wave.json',
-                          height: 70,
-                          animate: newsController.isSpeaking.value,
-                        ),
-                      ))
+                      Text(
+                        newsD.publishedAt!,
+                        style: Theme.of(context).textTheme.caption,
+                      )
                     ],
                   ),
-                ),
-                const SizedBox(height: 5),
-                Row(
-                  children: [
-                    Flexible(
-                      child: Text(
-                        newsD.description!,
-                        // "Midjourney says it has banned Stability AI staffers from using its service, accusing employees at the rival generative AI company of causing a systems outage earlier this month during an attempt to scrape Midjourney’s data.Midjourney posted an update to its Discord server on March 2nd that acknowledged an extended server outage was preventing generated images from appearing in user galleries. In a summary of a business update call on March 6th, Midjourney claimed that “botnet-like activity from paid accounts” — which the company specifically links to Stability AI employees — was behind the outage.",
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 15,
+                        backgroundImage: AssetImage('assets/images/image.png'),
+                        child: Text(
+                          "",
+                          style: const TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Text(
+                        newsD.author ?? "Gourav Dhiman",
                         style: TextStyle(fontSize: 18, color: Colors.black),
                       ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primaryContainer,
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                  ],
-                ),
-              ],
+                    child: Row(
+                      children: [
+                        Obx(
+                          () => newsController.isSpeaking.value
+                              ? IconButton(
+                                  onPressed: () {
+                                    newsController.stop();
+                                  },
+                                  icon: Icon(
+                                    Icons.stop,
+                                    size: 50,
+                                  ),
+                                )
+                              : IconButton(
+                                  onPressed: () {
+                                    newsController.speak(
+                                        newsD.description ?? "No Description");
+                                  },
+                                  icon: Icon(
+                                    Icons.play_arrow_rounded,
+                                    size: 50,
+                                  ),
+                                ),
+                        ),
+                        Expanded(
+                          child: Obx(
+                            () => Lottie.asset(
+                              'assets/animation/wave.json',
+                              height: 70,
+                              animate: newsController.isSpeaking.value,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Row(
+                    children: [
+                      Flexible(
+                        child: Text(
+                          newsD.description!,
+                          style: TextStyle(fontSize: 18, color: Colors.black),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
