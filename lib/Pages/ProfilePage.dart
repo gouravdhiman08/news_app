44478d650682455e0661ce179ea46b1918db6b14
Gourav Dhiman <techstudy.gourav@gmail.com>
2024-03-26 11:02:40 +0530

    Profile Page & Newsletter Feature added...

--- lib/Pages/ProfilePage.dart
+++ lib/Pages/ProfilePage.dart
@@ -1,34 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Profile Page")),
       body: Padding(
         padding: const EdgeInsets.all(18.0),
-        child: Column(
-          children: [
-            Container(
-              child: Text(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
                 "Subscribe to Our Newsletter!",
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-            ),
-            new Image.asset("assets/images/newsletter.jpg"),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                border: Border
-                    .all(), // Optionally, you can specify border properties
+              SizedBox(height: 20),
+              AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Image.asset("assets/images/newsletter.jpg",
+                    fit: BoxFit.cover),
               ),
-              child: TextField(
-                  // TextField properties...
+              SizedBox(height: 20),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter your email',
+                    border: InputBorder.none,
                   ),
-            )
-          ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Get.dialog(
+                      AlertDialog(
+                        title: Text('Alert'),
+                        content: Text('Subscribe to our newsletter?'),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () {
+                              // Close Alert Dialog
+                              Get.back();
+                            },
+                            child: Text('Cancel'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              // Perform subscription logic
+                              // Close Alert Dialog
+                              Get.back();
+                            },
+                            child: Text('Subscribe'),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  child: Text("Subscribe to our newsletter"),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
